2695a2aadba514b5e6f455929073871dde3c1268
lyl <419314472@qq.com>
2019-07-23 01:02:15 +0800

    add fuction "maxpool conv_layer" in models

--- tensorflow_lib/models.py
+++ tensorflow_lib/models.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import math
 
 
-def weight_variable(shape,trainable = True,f_in = None,f_out = None):
+def weight_variable(shape,trainable = True,f_in = None,f_out = None,name = None):
     '''
     
     :param shape:  a list that describe the shape of tensor
@@ -41,4 +41,37 @@ def fc( name, input_data, out_channel, trainable=True):
         biases = bias_variable(shape = [out_channel],trainable=trainable)
         res = tf.matmul(input_data_flat, weights)
         out = tf.nn.relu(tf.nn.bias_add(res, biases))
+    return out
+def maxpool(name, input_data ,height = 2, width = 2 ,strides = 2):
+    """
+    
+    :param name: 
+    :param input_data: a tensor
+    :param height: pool 's height
+    :param width:  pool 's width
+    :param strides: step len
+    :return: a tensor 
+    """
+    out = tf.nn.max_pool(input_data, [1,height, width, 1], [1, strides, strides, 1], padding="SAME", name=name)
+    return out
+def conv_layer(name, input_data, out_channel, trainable = True,strides = 1):
+    '''
+    卷积层
+    :param name: 卷积层名
+    :param input_data: 输入的张量
+    :param out_channel: 输出的轨道数
+    :param trainable: 是否训练？
+    :param strides: 步长
+    :return: 一个张量
+    '''
+    in_channel = input_data.get_shape()[-1]  # 从input张量中推测输入道数
+    with tf.variable_scope(name):
+        kernel = weight_variable([3, 3, in_channel, out_channel],trainable=trainable)
+            # tf.get_variable("weights", [3, 3, in_channel, out_channel], dtype=tf.float32,
+            #                      trainable=trainable)  # 权重
+        biases = bias_variable([out_channel],trainable=trainable)
+            # tf.get_variable("biases", [out_channel], dtype=tf.float32, trainable=trainable)
+        conv_res = tf.nn.conv2d(input_data, kernel, [1, strides, strides, 1], padding="SAME")
+        res = tf.nn.bias_add(conv_res, biases)
+        out = tf.nn.relu(res, name=name)
     return out
